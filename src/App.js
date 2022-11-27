2388040fd18a62a8c065ba4cec0f93cc72e49a36
conglechi93 <66486095+conglechi93@users.noreply.github.com>
2022-11-27 17:52:57 +0700

    up source

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="form-header">
+          <div className='form-column'>AV</div>
+          <div className='form-column'>AV</div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
