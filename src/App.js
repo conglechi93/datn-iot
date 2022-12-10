e5efd76ef7c73e252739dec03692f185236a47ae
conglechi93 <66486095+conglechi93@users.noreply.github.com>
2022-12-10 13:23:37 +0700

    UI header 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Switch } from 'antd';
+import { Button, Input, Switch } from 'antd';
 // import { initializeApp } from 'firebase/app';
 // import { getAnalytics } from "firebase/analytics";
 // import { getFirestore, 
@@ -7,7 +7,12 @@ import { Switch } from 'antd';
 // } from 'firebase/firestore/lite';
 import { useEffect, useState } from "react";
 import db from './firebaseConfig.js'
-
+import FanOnIcon from './image/fan_on.png';
+import FanOffIcon from './image/fan_off.png';
+import MistOnIcon from './image/mist_on.png';
+import MistOffIcon from './image/mist_off.png';
+import PinOnIcon from './image/pin_on.png';
+import PinOffIcon from './image/pin_off.png';
 
 
 
@@ -21,12 +26,39 @@ function App() {
   //   appId: "1:258914601635:web:5cb1b3da7718815c408d16",
   //   measurementId: "G-4GC4RKJL6T"
   // };
+  const [srcFan, setSrcFan] = useState();
+  const [srcMist, setSrcMist] = useState();
+  const [srcPin1, setSrcPin1] = useState();
+  const [srcPin2, setSrcPin2] = useState();
+  // const [srcFan, setSrcFan] = useState();
+  // const [srcFan, setSrcFan] = useState()
+
+  const onChangeFan = (e) => {
+    console.log(e)
+    if(e) setSrcFan(FanOnIcon)
+    else setSrcFan(FanOffIcon)
+  }
+
+  const onChangeMist = (e) => {
+    console.log(e)
+    if(e) setSrcMist(MistOnIcon)
+    else setSrcMist(MistOffIcon);
+  }
 
 
   
   // Initialize Firebase
-  const onChange = (e) => {
+  const onChangePin1 = (e) => {
     console.log(e);
+    if(e) setSrcPin1(PinOnIcon)
+    else setSrcPin1(PinOnIcon);
+    
+  };
+
+  const onChangePin2 = (e) => {
+    if(e) setSrcPin2(PinOnIcon)
+    else setSrcPin2(PinOffIcon);
+    
   };
 
   const [blogs,setBlogs]=useState([])
@@ -39,11 +71,81 @@ function App() {
   }
   useEffect(() => {
     fetchBlogs();
+    setSrcFan(FanOnIcon)
   }, [])
 
   return (
     <div className="App">
       <header className="App-header">
+        <div className='form-tilte'>
+          <div> Điều khiển nhiệt độ, độ ẩm giám sát chuồng trại chăn nuôi heo</div>
+          <div> Luận văn tốt nghiệp 2022 </div>
+        </div>
+
+        <div className='form-header'>
+          <div className='column-1'>
+            <div>Chọn chế độ</div>
+            <div style={{display:"flex", justifyContent:"space-between", margin:'auto', textAlign:"center"}}>
+              <p>AUTO</p>
+              <Switch defaultChecked style={{margin:"auto"}}/>
+            </div>
+            <div style={{marginBottom:"20px"}}>Chọn giá trị</div>
+            <div style={{display:"flex", justifyContent:"space-between",textAlign:"center"}}>
+              <Input className='input-type'/>
+              <Button className='button-type'>Thiết lập</Button>
+            </div>   
+          </div>
+          <div className='column-2'>
+            <div className='footer'>
+              <div className='footer-child'>
+                <p>ĐÈN 1-2</p>
+                <Switch defaultChecked onChange={onChangePin1} />
+                <div className='form-img'>
+                  <img className='img-ico' src={srcPin1}></img>
+                </div>
+                
+              </div>
+              <div className='footer-child'>
+                <p>ĐÈN 3-4</p>
+                <Switch defaultChecked onChange={onChangePin2} />
+                <div className='form-img'>
+                  <img className='img-ico' src={srcPin2}></img>
+                </div>
+              </div>
+              <div className='footer-child'>
+                <p>QUẠT</p>
+                <Switch defaultChecked onChange={onChangeFan} />
+                <div className='form-img'>
+                  <img className='img-ico' src={srcFan}></img>
+                </div>
+              </div>
+              <div className='footer-child'>
+                <p>PHUN SƯƠNG</p>
+                <Switch defaultChecked onChange={onChangeMist} />
+                <div className='form-img'>
+                  <img className='img-ico' src={srcMist}></img>
+                </div>
+                {
+                  blogs && blogs.map(blog=>{
+                    return(
+                      <div className="blog-container">
+                        <h4>{blog.title}</h4>
+                        <p>{blog.body}</p>
+                      </div>
+                    )
+                  })
+                }
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className='form-header'>
+          <div>SOUND WAVE</div>
+        </div>
+
+
         <div className="form-header">
           <div className='form-column'>
             <div className='content-title'>
@@ -71,38 +173,7 @@ function App() {
           <div className='form-column'>AV</div>
         </div>
 
-      <div className='footer'>
-        <div className='footer-child'>
-          <p>RELAY 1</p>
-          <p>{"(ĐẤU CHUNG ĐÈN 2 ĐÈN 1 RELAY)"}</p>
-          <Switch defaultChecked onChange={onChange} />
-        </div>
-        <div className='footer-child'>
-          <p>RELAY 2</p>
-          <p>{"(ĐẤU CHUNG ĐÈN 2 ĐÈN 1 RELAY)"}</p>
-          <Switch defaultChecked onChange={onChange} />
-        </div>
-        <div className='footer-child'>
-          <p>RELAY 3</p>
-          <p>{"(ĐẤU CHUNG ĐÈN 2 QUẠT 1 RELAY)"}</p>
-          <Switch defaultChecked onChange={onChange} />
-        </div>
-        <div className='footer-child'>
-          <p>RELAY 4</p>
-          <p>{"(ĐẤU CHUNG MOTOR PHUN SƯƠNG)"}</p>
-          <Switch defaultChecked onChange={onChange} />
-          {
-            blogs && blogs.map(blog=>{
-              return(
-                <div className="blog-container">
-                  <h4>{blog.title}</h4>
-                  <p>{blog.body}</p>
-                </div>
-              )
-            })
-          }
-        </div>
-      </div>
+      
       </header>
     </div>
   );
